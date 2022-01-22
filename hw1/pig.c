2ea8d838b68e73f8f3ae82907a50e0ae37c16da3
Granger <gpasko@ucsc.edu>
2022-01-21 17:38:04 -0800

    almsot done with pig, need to finish after volleyball or on sunday

--- hw1/pig.c
+++ hw1/pig.c
@@ -15,47 +15,90 @@ const Position pig [7] = {
 };
 
 int main(void){
-	int num_players;
+	int num_players = 0;
+	int num_players_input;
+	int prev_player = 8;
 	printf("num_players initialzied\n");
 	printf("Enter number of players: \n");
-	scanf("%d", &num_players);
+	scanf("%d", &num_players_input);
 	if(!(num_players < 11) && (num_players > 1)){
 		num_players = 2;
 		fprintf(stderr, "Invalid number of players. Using 2 instead .\n");
 	}
-	printf("num players defined\n");
+	else{
+		num_players = num_players_input;
+		//printf("num players defined as %d\n", num_players);
+	}
 	
 	int seed = 0;
+	int seed_input;
 	printf("Enter seed: \n");
-	scanf("%d", &seed);
-	if(!(seed < INT_MAX && seed > 0)){
-		fprintf(stderr, "Invalid ranodm seed. Using 2022 instead.\n");
-		seed = 2022;
+	scanf("%d", &seed_input);
+	//printf("seed is: %d and seed_input is %d\n", seed, seed_input);
+	if((seed_input < INT_MAX) && (seed_input > 0)){
+		seed = seed_input;
+                //printf("seed defined as %d\n", seed);
+
+		//fprintf(stderr, "Invalid ranodm seed. Using 2022 instead.\n");
+		//seed = 2022;
 	}
+	else{
+		fprintf(stderr, "Invalid ranodm seed. Using 2022 instead.\n");
+                seed = 2022;
+
+		//seed = seed_input;
+		//printf("seed initialzized as %d", seed);
+	}
+	srandom(seed);
 	int points[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	int player_index = 0;
 	while(player_index < (num_players + 1)){
-		printf("%s", names[player_index]);
+		//printf("player_index: %d. prev_player: %d\n", player_index, prev_player);
+		if(player_index != prev_player){
+			printf("%s rolls the pig...", names[player_index]);
+		}
 		int roll = (random() % 7);
 		switch(pig[roll]){
 			case SIDE:
-				;
+				printf("pig lands on side\n");
+				if(points[player_index] >= 100){
+					printf("%s wins with %d points!\n", names[player_index], points[player_index]);
+					return 0;
+				}
+				prev_player = player_index;
+				player_index += 1;
+				prev_player = player_index;
+				break;
 			case RAZORBACK:
+				printf("pig lands on back ");
 				points[player_index] += 10;
+				prev_player = player_index;
+				break;
 			case TROTTER:
+				printf("pig lands upright ");
 				points[player_index] +=10;
+				prev_player = player_index;
+				break;
 			case SNOUTER:
+				printf("pig lands on snout ");
 				points[player_index] += 15;
+				prev_player = player_index;
+				break;
 			case JOWLER:
+				printf("pig lands on ear ");
 				points[player_index] += 5;
+				prev_player = player_index;
+				break;
 			}
-		if(points[player_index] >= 100){
-			printf("Congradulations %s\n", names[player_index]);
-		break;
-		}
+		
+		//if(points[player_index] >= 100){
+			//printf("Congradulations %s\n", names[player_index]);
+			//break;		
+		//}
+		
+		if(player_index == num_players){
+			player_index = 0;
+		} 	
 	}
 	return 0;
 }
-		
-
-
